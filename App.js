98590bc9cd799f42d788a796f511faf29f9fbe62
Tola Olusheye <47742179+tolaolu@users.noreply.github.com>
2021-07-14 11:51:28 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,7 @@
+import React from "react";
+//import ViewImageScreen from "./app/assets/screens/ViewImageScreen";
+import WelcomeScreen from "./app/assets/screens/WelcomeScreen";
+
+export default function App() {
+  return <WelcomeScreen />;
+}
